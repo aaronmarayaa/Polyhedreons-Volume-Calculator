cf84d45f0964070bcc9b65e610dac8bd7c2ddb76
Aaron Maraya <aaronmaraya7@gmail.com>
2023-07-17 13:26:54 +0800

    feat: Add formula to solve for volume and area of a cube
    Path :
    - shapes/cube/cube.js

--- shapes/cube/cube.js
+++ shapes/cube/cube.js
@@ -1,1 +1,9 @@
-console.log("Hello Cube!")
+const edge = document.querySelector("#edge");
+const volume_result = document.querySelector("#volume-result");
+const area_result = document.querySelector("#area-result");
+const solve_btn = document.querySelector("#solve-btn");
+
+solve_btn.onclick = (evt) => {
+  volume_result.innerHTML = edge.value ** 3;
+  area_result.innerHTML = 6 * edge.value ** 2;
+};
