6d3dbac6b905887a288cf142a2411b1ab8a625f5
Aaron Maraya <aaronmaraya7@gmail.com>
2023-08-02 15:24:03 +0800

    feat: Add formula to solve for volume of triangular prism.
    Path :
    - shapes/triangularPrism/triangularPrism.js

---
+++ shapes/triangularPrism/triangularPrism.js
@@ -0,0 +1,25 @@
+const base_sideA = document.querySelector('#base-sideA')
+const base_sideB = document.querySelector('#base-sideB')
+const base_sideC = document.querySelector('#base-sideC')
+const height = document.querySelector('#height')
+const volume_result = document.querySelector('#volume-result')
+const solve_btn = document.querySelector('#solve-btn')
+
+solve_btn.onclick = () => {
+    const baseSide_A = parseFloat(base_sideA.value)
+    const baseSide_B = parseFloat(base_sideB.value)
+    const baseSide_C = parseFloat(base_sideC.value)
+    const _height = parseFloat(height.value)
+
+    getVolume(baseSide_A, baseSide_B, baseSide_C, _height)
+}
+
+function getVolume(a, b, c, h) {
+    const term1 = (a + b + c )
+    const term2 = (b + c - a)
+    const term3 = (c + a - b)
+    const term4 = (a + b - c)
+    const volume_value = (1/4) * h * Math.sqrt(term1 * term2 * term3 * term4)
+
+    volume_result.innerHTML = volume_value.toFixed(4)
+}
