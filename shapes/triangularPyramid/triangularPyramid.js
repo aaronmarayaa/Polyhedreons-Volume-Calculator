a68ae6f97738aec66e7849a59b33a408340512c4
Aaron Maraya <aaronmaraya7@gmail.com>
2023-08-24 22:20:16 +0800

    feat: Add formula to solve for volume of triangular pyramid.
    file :
    - shapes/triangularPyramid/triangularPyramid.js

--- shapes/triangularPyramid/triangularPyramid.js
+++ shapes/triangularPyramid/triangularPyramid.js
@@ -1,1 +1,19 @@
-console.log("Triangular Pyramid!")
+const volume_result = document.querySelector('#volume-result')
+const solve_btn = document.querySelector('#solve-btn')
+
+solve_btn.onclick = () => {
+    const height = parseFloat(document.querySelector('#height').value)
+    const base = parseFloat(document.querySelector('#base').value)
+    const length = parseFloat(document.querySelector('#length').value)
+
+    getVolume(height, base, length)
+}
+
+function getVolume(h, b, l) {
+    // h = height
+    // b = base
+    // l = length
+
+    const volume_value = (1 / 2) * (b * h * l)
+    volume_result.innerHTML = volume_value.toFixed(4)
+}
