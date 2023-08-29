a52e62de00de643c32af5065032480499f5f8c3d
Aaron Maraya <aaronmaraya7@gmail.com>
2023-08-29 22:23:11 +0800

    feat: Add formula to solve for volume and area of a sphere sector.
    file :
    - shapes/sphereSector/sphereSector.js

--- shapes/sphereSector/sphereSector.js
+++ shapes/sphereSector/sphereSector.js
@@ -1,1 +1,29 @@
-console.log("Sphere Sector!")
+const volume_result = document.querySelector('#volume-result')
+const area_result = document.querySelector('#area-result')
+const solve_btn = document.querySelector('#solve-btn')
+const pi = Math.PI
+
+solve_btn.onclick = () => {
+    const radius = parseFloat(document.querySelector('#radius').value)
+    const height = parseFloat(document.querySelector('#height').value)
+
+    getVolume(radius, height)
+    getArea(radius, height)
+}
+
+function getVolume(r, h) {
+    // r = radius
+    // h = height
+
+    const volume_value = (2/3) * pi * r**2 * h
+    volume_result.innerHTML = volume_value.toFixed(4)
+}
+
+function getArea(r, h) {
+    // r = radius
+    // h = height
+    
+    const a = Math.sqrt(h * (2 * r - h)) // sphere cap radius
+    const area_value = pi * r * ( 2 * h + a )
+    area_result.innerHTML = area_value.toFixed(4)
+}
